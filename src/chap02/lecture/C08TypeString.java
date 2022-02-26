14ae6c7b1e43a190d8e6386fbcf7f8060b0c3d48
JamesHJ0137 <99157645+JamesHJ0137@users.noreply.github.com>
2022-02-27 00:57:04 +0900

    Variable and Type

---
+++ src/chap02/lecture/C08TypeString.java
@@ -0,0 +1,14 @@
+package chap02.lecture;
+
+public class C08TypeString {
+	public static void main(String[] args) {
+		// 문자열 String
+		String var1 = "";
+		String var2 = "A";
+		String var3 = "hello world!";
+		String var4 = "❤️";
+		String var5 = "hello \tworld";
+		String var6 = "hello\nworld";
+		String var7 = "I am \"ironman\"";
+	}
+}
