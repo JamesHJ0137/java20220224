7d784fdb0b10c894507b8972edb00a9226ee1dda
JamesHJ0137 <99157645+JamesHJ0137@users.noreply.github.com>
2022-03-15 21:00:14 +0900

    Inheritance Override super final

---
+++ src/chap07/book/s070600/package1/B.java
@@ -0,0 +1,9 @@
+package chap07.book.s070600.package1;
+
+public class B {
+	public void method() {
+		A a = new A();
+		a.field = "value";
+		a.method();
+	}
+}
---
+++ src/chap07/book/s070600/package2/D.java
@@ -0,0 +1,11 @@
+package chap07.book.s070600.package2;
+
+import chap07.book.s070600.package1.A;
+
+public class D extends A {
+	public D() {
+		super(); 				//o
+		this.field = "field";   //o
+		this.method();			//o
+	}
+}
