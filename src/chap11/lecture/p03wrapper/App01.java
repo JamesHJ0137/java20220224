397cccd34e5ce92418fcb33cf8de926477f4d2a4
JamesHJ0137 <99157645+JamesHJ0137@users.noreply.github.com>
2022-03-23 20:16:20 +0900

    wrapper enum

---
+++ src/chap11/lecture/p03wrapper/App01.java
@@ -0,0 +1,18 @@
+package chap11.lecture.p03wrapper;
+
+public class App01 {
+	public static void main(String[] args) {
+		// java data type
+		// primitive type (8) : byte, short, int, long, char, float, double, boolean
+		// reference type : 나머지 모두 다
+		
+		
+		Object[] arr = new Object[3];
+		arr[0] = "java";
+		arr[1] = 3; // java 5부터 가능
+		arr[2] = new Integer(3); // java 4
+		
+		// wrapper class (8)
+		// Byte, Short, Integer, Long, Character, Float, Double, Boolean
+	}
+}
